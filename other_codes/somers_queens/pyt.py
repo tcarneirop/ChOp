85fb951a2416554ca19498e20dfcfa6ac66c91cf
Tiago Carneiro <carneirouece@gmail.com>
2022-07-03 00:15:41 +0200

    adding jeff somers queens to the other codes

---
+++ other_codes/somers_queens/pyt.py
@@ -0,0 +1,13 @@
+sol = 0
+def queens(n, i, a, b, c):
+	if i < n:
+		for j in range(n):
+			if j not in a and i+j not in b and i-j not in c:
+				yield from queens(n, i+1, a+[j], b+[i+j], c+[i-j])
+	else:
+		yield a
+
+for solution in queens(13, 0, [], [], []):
+	sol = sol + 1
+
+print(sol)
