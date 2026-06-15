6e59fc0ba7d467a01a1836b21d879615ecf72e52
Tiago Carneiro Pessoa <tcarneiropessoa@fluxembourg.luxembourg.grid5000.fr>
2026-06-15 13:37:17 +0200

    adding a script to build chapel 2.8 multilocale on grid5k

---
+++ runtimeConfigScripts/v2cfgs/v2_lux_UDP_AMD.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo -e \#\#\# Building Chapel runtime 2.80 AMD GPU - $(rocm_agent_enumerator | grep -v gfx000 | sort -u | head -1) -  Multi Locale - $CHPL_COMM_SUBSTRATE   \#\#\#
+echo " ### exporting...  ### "
+
+ml cmake/3.23.3_gcc-10.4.0
+ml gcc/13.2.0_gcc-10.4.0
+
+sudo-g5k apt-get update
+sudo-g5k apt-get install -y hipcc
+sudo-g5k apt-get install -y rocprim-dev hipcub-dev
+
+export LD_LIBRARY_PATH=$LLVM_ROOT/lib:/opt/rocm-6.3.3/lib:$LD_LIBRARY_PATH
+
+
+export HIP_PATH=/opt/rocm-6.3.3/
+export ROCM_PATH=/opt/rocm-6.3.3/
+export DEVICE_LIB_PATH=/opt/rocm-6.3.3/amdgcn/bitcode/
+export CHPL_HOME=~/chapel-2.8.0
+export CHPL_LLVM=bundled
+export CHPL_LOCALE_MODEL=gpu
+export CHPL_HOST_PLATFORM=`$CHPL_HOME/util/chplenv/chpl_platform.py`
+export CHPL_GPU=amd
+export CHOP_HOME=~/ChOp
+export CHPL_ROCM_PATH=/opt/rocm-6.3.3/
+export CHPL_GPU_ARCH=$(rocm_agent_enumerator | grep -v gfx000 | sort -u | head -1)
+export CHPL_GPU_MEM_STRATEGY=array_on_device
+#export CHPL_RT_NUM_GPUS_PER_LOCALE
+
+# use gasnet-ofi -- from the psm docs "Users of Intel(R) Omni-Path Fabric are
+  # recommended to use ofi-conduit"
+export CHPL_COMM='gasnet'
+export CHPL_COMM_SUBSTRATE='udp'
+export CHPL_TARGET_CPU='native'
+export GASNET_QUIET=1
+export GASNET_SPAWNFN=S
+export GASNET_SSH_CMD=ssh
+export GASNET_SSH_OPTIONS=-x
+export GASNET_PHYSMEM_MAX='0.667'
+export GASNET_SSH_SERVERS=$(uniq $OAR_NODEFILE | tr '\n' ' ')
+
+
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"$CHOP_HOME"/libs
+
+CHPL_BIN_SUBDIR=`"$CHPL_HOME"/util/chplenv/chpl_bin_subdir.py`
+export PATH="$PATH":"$CHPL_HOME/bin/$CHPL_BIN_SUBDIR"
+
+echo $LD_LIBRARY_PATH
+
+export MANPATH="$MANPATH":"$CHPL_HOME"/man
+
+
+NUM_T_LOCALE=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+export CHPL_TARGET_ARCH=native
+NUM_T_LOCALE=$(cat /proc/cpuinfo | grep processor | wc -l)
+export CHPL_RT_NUM_THREADS_PER_LOCALE=$NUM_T_LOCALE
+export CHPL_TASKS=qthreads
+
+echo -e \#\#\#QThreads set for $NUM_T_LOCALE threads\#\#\#.
+
+export here=$(pwd)
+
+echo $here
+
+
+cd $CHPL_HOME
+make -j $NUM_T_LOCALE
+echo -e \#\#\# DONE \#\#\# 
+echo -e \#\#\# Chapel runtime 2.80 AMD GPU - $(rocm_agent_enumerator | grep -v gfx000 | sort -u | head -1) - Multi Locale - $CHPL_COMM_SUBSTRATE \#\#\#
+
+cd $here
