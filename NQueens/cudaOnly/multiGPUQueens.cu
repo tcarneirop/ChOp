2ddf7dd590f8d8f1afd32696bae0e3c8dcb78449
Tiago Carneiro <carneirouece@gmail.com>
2026-06-26 18:26:00 +0200

    updating the cuda app name

---
+++ NQueens/cudaOnly/multiGPUQueens.cu
@@ -0,0 +1,43 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <omp.h>
+
+
+#include "../headers/timer.hpp"
+#include "../headers/queens_omp_aux.hpp"
+#include "../headers/queens_subproblem.hpp"
+#include "../headers/queens_CPU_GPU_subproblem_eval.hpp"
+#include "../headers/queens_GPU_enumeration.hpp"
+#include "../headers/queens_sub_gen.hpp"
+#include "cuda_headers/cuda_funcs.hpp"
+#include "../headers/queens_MGPU_call_queens.hpp"
+
+
+
+int main(int argc, char *argv[]){
+
+
+    int block_size;
+    int initialDepth;
+    int size;
+
+    #ifdef IMPROVED
+        printf("### IMPROVED SEARCH - Avoiding mirrored solutions\n");
+    #endif
+
+    if (argc != 4) {
+        printf("Usage: %s <size> <initial depth> <block size>\n", argv[0]);
+        return 1;
+    }
+
+    size = atoi(argv[1]);
+    initialDepth = atoi(argv[2]);
+    block_size   =   atoi(argv[3]);
+
+   MGPUcall_queens(size, initialDepth, block_size);
+
+    return 0;
+}
